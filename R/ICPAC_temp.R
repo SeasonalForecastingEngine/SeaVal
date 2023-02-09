1935efd5c68068d6c4a159fe82b0c6370ce7df1b
Claudio Heinrich <claudio@nr.no>
2023-02-09 14:59:17 +0100

    make get_mask smarter

--- R/ICPAC_temp.R
+++ R/ICPAC_temp.R
@@ -148,14 +148,29 @@ get_mask = function( season,
                      us = (resolution == 'low'))
 
 {
-  mms = season_months(season)
-  dt = load_chirps(years = clim_years,months = NULL,us = us,version = version)
-  clim = dt[,.(clim = mean(prec)),.(lon,lat)]
-  season_average = dt[month %in% mms,.(season_average = mean(prec)),.(lon,lat)]
-  dt = merge(clim,season_average,c('lon','lat'))
-  dt[,fraction := (season_average*length(mms))/(clim*12)]
-  dt[,mask:= fraction <= 0.1]
-  return(dt[(!mask),.(lon,lat)])
+  ch_dir = file.path(chirps_dir(),version)
+  if (us) ch_dir = file.path(ch_dir,'upscaled')
+  if (!dir.exists(ch_dir)) stop("I didn't find the directory with the CHIRPS data. Have you downloaded/upscaled CHIRPS? If not, run download_chirps_monthly.")
+  ch_dir = file.path(ch_dir,'masks')
+  dir.create(ch_dir,showWarnings = FALSE)
+
+  fn = file.path(ch_dir,paste0(season,'_',min(clim_years),'_',max(clim_years),'.csv'))
+
+  if(file.exists(fn)) {
+    dt = fread(fn)
+    return(dt)
+  } else {
+    mms = season_months(season)
+    dt = load_chirps(years = clim_years,months = NULL,us = us,version = version)
+    clim = dt[,.(clim = mean(prec)),.(lon,lat)]
+    season_average = dt[month %in% mms,.(season_average = mean(prec)),.(lon,lat)]
+    dt = merge(clim,season_average,c('lon','lat'))
+    dt[,fraction := (season_average*length(mms))/(clim*12)]
+    dt[,mask:= fraction <= 0.1]
+    #save:
+    fwrite(dt[(!mask),.(lon,lat)],file = fn)
+    return(dt[(!mask),.(lon,lat)])
+  }
 }
 
 
